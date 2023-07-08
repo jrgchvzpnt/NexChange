24f8cb2e3eaf30756a15cf3ef813785c17f8749b
Jorge Chavez <jorge.puente@aforecoppel.com>
2023-07-07 17:08:32 -0700

    Primer version

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Ejemplo de script en JavaScript
+
+// Espera a que se cargue el contenido de la página
+document.addEventListener('DOMContentLoaded', function() {
+    // Agrega interactividad aquí
+  
+    // Ejemplo: Cambiar el color de fondo del botón al hacer clic
+    var btn = document.querySelector('.btn');
+    btn.addEventListener('click', function() {
+      btn.style.backgroundColor = '#cc5500';
+    });
+  });
+  
